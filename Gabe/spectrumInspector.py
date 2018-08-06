057877863655517c6f8158e945dd76456287bf27
Gabe Alvarez <alvarez.gabe11@gmail.com>
2018-08-06 10:14:26 -0500

    more data

--- Gabe/spectrumInspector.py
+++ Gabe/spectrumInspector.py
@@ -9,9 +9,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 files = {}
-files.update({"around AOM but through trap, split onto both PDs":"../data/rawdata/2018_08_02_4_n.txt"})
-
+#files.update({"around AOM but through trap, split onto both PDs":"../data/rawdata/2018_08_02_4_n.txt"})
 #files.update({"with vacuum pump":"../data/rawdata/2018_08_02_2_n.txt","without vacuum pump":"../data/rawdata/2018_08_02_3_n.txt"})
+files.update({"AOMs off":"../data/rawdata/2018_08_06_1_n.txt","AOMs on":"../data/rawdata/2018_08_06_2_n.txt"})
 
 def fourier(data,samplingFreq):
     print 'running fourier analysis...'
@@ -31,10 +31,10 @@ for i in files:
     input_file.close()
     
     freq_, fft_ = fourier(sample,samplingFreq_)
-
+    #plt.figure()
     plt.plot(freq_,np.power(np.abs(fft_),2),",",label=i)
     plt.title(i)
     plt.xlabel("frequency (Hz)")
     plt.xscale("log")
     plt.yscale("log")
-    #plt.legend()
+    plt.legend()
